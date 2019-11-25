729b8713ffaf30262eadfc9e492045f8ff91b0f5
JCWarmels <jesse.warmels@student.rocva.nl>
2019-11-25 14:27:34 +0100

    Opdracht 3 af

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,20 @@
 //javascript code
+
+function mysaldo()
+{
+
+var saldo = 520;
+
+    if (saldo <= 499)
+        {
+            document.getElementById("melding").innerHTML="U heeft geen Salaris ontvangen.";
+        }
+    else if (saldo == 500)
+        { 
+            document.getElementById("melding-1").innerHTML="U heeft Salaris ontvangen.";
+        }
+    else 
+        {
+            document.getElementById("melding-2").innerHTML="U heeft Salaris en een Bonus ontvangen.";
+        }
+}
