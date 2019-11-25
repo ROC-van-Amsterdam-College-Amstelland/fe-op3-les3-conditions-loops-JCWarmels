5fae58302b25e1d7917d03a385b58bdb10032a34
JCWarmels <jesse.warmels@student.rocva.nl>
2019-11-25 13:49:38 +0100

    Opdracht 2 klaar

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+
+function mysaldo()
+{
+
+var saldo= 4;
+
+if(saldo<25)
+    {
+        document.getElementById("melding").innerHTML="U heeft te weinig saldo.";
+    }
+else 
+    { 
+        document.getElementById("melding-1").innerHTML="25 euro wordt uitgegeven";
+    }
+}
