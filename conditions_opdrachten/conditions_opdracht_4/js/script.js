b2a7fe13225edc771ac6605d276020c05ec300dc
JCWarmels <jesse.warmels@student.rocva.nl>
2019-11-25 14:38:29 +0100

    Opdracht 4 af

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+
+function waarde()
+{
+
+var myNumber = 10
+
+if(myNumber = 10)
+    {
+        document.getElementById("melding").innerHTML="De variabele myNumber heeft de waarde: 10"
+    }
+}
