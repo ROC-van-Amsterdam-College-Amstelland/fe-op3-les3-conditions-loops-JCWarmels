71a097d6274cb339ba0df75234a96c3e7af1c115
JCWarmels <jesse.warmels@student.rocva.nl>
2019-11-27 11:53:25 +0100

    Opdracht 6 af

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,26 @@
 //javascript code
+function calculator(myOperator)
+{
+var myNumber1 = parseInt(document.getElementById("myNumber1").value);
+var myNumber2 = parseInt(document.getElementById("myNumber2").value);
+var myUitkomst;
+
+    if(myOperator == "+")
+    {
+        myUitkomst = myNumber1 + myNumber2;
+    }
+    else if(myOperator == "*")
+    {
+        myUitkomst = myNumber1 * myNumber2;
+    }
+    else if(myOperator == "/")
+    {
+        myUitkomst = myNumber1 / myNumber2;
+    }
+    else
+    {
+        myUitkomst = myNumber1 - myNumber2;
+    }
+
+    document.getElementById("myUitkomst").innerHTML="De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+}
