4a6fd6f3f75fbea24dd49ce2cb3d835e904d931f
JCWarmels <jesse.warmels@student.rocva.nl>
2019-11-27 11:41:58 +0100

    Opdracht 5 af

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,18 @@
 //javascript code
+function calculator(myOperator)
+{
+var myNumber1 = parseInt(document.getElementById("myNumber1").value);
+var myNumber2 = parseInt(document.getElementById("myNumber2").value);
+var myUitkomst;
+
+    if(myOperator == "+")
+    {
+        myUitkomst = myNumber1 + myNumber2;
+    }
+    else
+    {
+        myUitkomst = myNumber1 - myNumber2;
+    }
+
+    document.getElementById("myUitkomst").innerHTML="De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
+}
