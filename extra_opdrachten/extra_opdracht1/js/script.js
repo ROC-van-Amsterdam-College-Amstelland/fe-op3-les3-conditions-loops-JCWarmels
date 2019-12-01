7d39511477c5ea14c5f5f17e70f61f9f8b075710
JCWarmels <jesse.warmels@student.rocva.nl>
2019-12-01 16:33:47 +0100

    Test

--- extra_opdrachten/extra_opdracht1/js/script.js
+++ extra_opdrachten/extra_opdracht1/js/script.js
@@ -1,30 +1,5 @@
 //javascript code
 function calculator(myOperator)
 {
-var myDisplay = parseInt(document.getElementById("myDisplay").value = myOperator);
-var myUitkomst;
 
-    if(myOperator == "C")
-    {
-        myUitkomst = "";
-    }
-    else if(myOperator == "*")
-    {
-        myUitkomst = myNumber1 * myNumber2;
-    }
-    else if(myOperator == "/")
-    {
-        myUitkomst = myNumber1 / myNumber2;
-    }
-    else
-    {
-        myUitkomst = myNumber1 - myNumber2;
-    }
-
-    document.getElementById("myUitkomst").innerHTML="De uitkomst van de som " + myNumber1 + myOperator + myNumber2 + " = " + myUitkomst;
 }
-function myDisplay()
-{
-    
-}
-
