282b3bff040be795f103bba99fd5935ba126ec17
JCWarmels <jesse.warmels@student.rocva.nl>
2019-12-01 17:45:00 +0100

    Sup

--- extra_opdrachten/extra_opdracht1/js/script.js
+++ extra_opdrachten/extra_opdracht1/js/script.js
@@ -1,5 +1,15 @@
 //javascript code
 function calculator(myOperator)
 {
+var myDisplay = myArrayFirst;
+parseInt(document.getElementById("myDisplay").innerHTML=(myDisplay));
+var myArray = [myNumber1, myNumber2]
+var myArrayFirst = myArray[0];
 
+    if(myOperator == '1')
+    {
+        myNumber1 = "1";
+    }
 }
+
+
