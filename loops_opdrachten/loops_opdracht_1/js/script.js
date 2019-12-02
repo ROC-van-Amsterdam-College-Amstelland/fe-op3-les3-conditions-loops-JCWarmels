3717dc901983b762df7269ab2cb8d5a453644f3d
JCWarmels <jesse.warmels@student.rocva.nl>
2019-12-02 14:39:39 +0100

    Opdracht 1 en 2 van loops af

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+function loop()
+{
+var i;
+var text = "";
+for (i = 1; i <= 5; i++) 
+{
+    text += "Het nummer is " + i + "<br>"
+}
+document.getElementById("een").innerHTML = text
+}
--- loops_opdrachten/loops_opdracht_2/js/script.js
+++ loops_opdrachten/loops_opdracht_2/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+function loop()
+{
+var i;
+var text = "";
+for (i = 20; i >= 0; i--) 
+{
+    text += "Het nummer is " + i + "<br>"
+}
+document.getElementById("een").innerHTML = text
+}
