c769ab079d974f0d8ac1d7e75d9ce113d0d614cb
JCWarmels <jesse.warmels@student.rocva.nl>
2019-12-02 13:16:11 +0100

    Sup

--- extra_opdrachten/extra_opdracht1/js/script.js
+++ extra_opdrachten/extra_opdracht1/js/script.js
@@ -1,15 +1,23 @@
 //javascript code
-function calculator(myOperator)
+function calculator(myInput)
 {
-var myDisplay = myArrayFirst;
-parseInt(document.getElementById("myDisplay").innerHTML=(myDisplay));
-var myArray = [myNumber1, myNumber2]
-var myArrayFirst = myArray[0];
-
-    if(myOperator == '1')
+    if(Number.isInteger(myInput)==true)
     {
-        myNumber1 = "1";
+        console.log("number");
+    } 
+    else if(myInput == ".")
+    {
+        console.log("break");
+    } 
+    else
+    {
+       console.log("operation");
     }
+
+//var myDisplay = myArrayFirst;
+//parseInt(document.getElementById("myDisplay").innerHTML=(myDisplay));
+//var myArray = [myNumber1, myNumber2]
+//var myArrayFirst = myArray[0];
 }
 
 
